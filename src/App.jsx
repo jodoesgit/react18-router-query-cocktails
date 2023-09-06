5bf6e9777f7897a68d34a00a03da752ed6e5b28a
jodoesgit <122350666+jodoesgit@users.noreply.github.com>
2023-09-06 14:40:54 -0700

    Nested pages to use Outlet function w/ nav

--- src/App.jsx
+++ src/App.jsx
@@ -12,42 +12,44 @@ const router = createBrowserRouter([
 	{
 		path: "/",
 		element: <HomeLayout />,
-	},
-	{
-		path: "/about",
-		element: <About />,
-	},
-	{
-		path: "/error",
-		element: (
-			<div>
-				<Error />
-			</div>
-		),
-	},
-	{
-		path: "/Landing",
-		element: (
-			<div>
-				<Landing />
-			</div>
-		),
-	},
-	{
-		path: "/cocktail",
-		element: (
-			<div>
-				<Cocktail />
-			</div>
-		),
-	},
-	{
-		path: "/newsletter",
-		element: (
-			<div>
-				<Newsletter />
-			</div>
-		),
+		children: [
+			{
+				path: "about",
+				element: <About />,
+			},
+			{
+				path: "error",
+				element: (
+					<div>
+						<Error />
+					</div>
+				),
+			},
+			{
+				index: true,
+				element: (
+					<div>
+						<Landing />
+					</div>
+				),
+			},
+			{
+				path: "cocktail",
+				element: (
+					<div>
+						<Cocktail />
+					</div>
+				),
+			},
+			{
+				path: "newsletter",
+				element: (
+					<div>
+						<Newsletter />
+					</div>
+				),
+			},
+		],
 	},
 ]);
 
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,7 @@
-import { Link } from "react-router-dom";
-
 const About = () => {
 	return (
 		<div>
-			<h1>HomeLayout</h1>
-			<Link to="/">Home Page</Link>
+			<h1>About</h1>
 		</div>
 	);
 };
