2ce895024265fb9ef199d5de54a33493ca7b3f5e
jodoesgit <122350666+jodoesgit@users.noreply.github.com>
2023-09-06 18:44:24 -0700

    Mapped over data, populated on site

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,17 +1,23 @@
 import { useLoaderData } from "react-router-dom";
 import axios from "axios";
+import Cocktail from "./Cocktail";
+import CocktailList from "../components/CocktailList";
 
 const cocktailSearchUrl =
 	"https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
 
 export const loader = async () => {
-	const searchTerm = "margarita";
+	const searchTerm = "";
 	const response = await axios.get(`${cocktailSearchUrl}${searchTerm}`);
 	return { drinks: response.data.drinks, searchTerm };
 };
 
 const Landing = () => {
 	const { drinks, searchTerm } = useLoaderData();
-	return <h1>Landing</h1>;
+	return (
+		<>
+			<CocktailList drinks={drinks} />
+		</>
+	);
 };
 export default Landing;
