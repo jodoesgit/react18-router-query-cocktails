6b5a220398ea0fdb76a6ebe8e4a2726847e185be
jodoesgit <122350666+jodoesgit@users.noreply.github.com>
2023-09-10 13:51:07 -0700

    Added newsletter formatting

--- src/pages/Newsletter.jsx
+++ src/pages/Newsletter.jsx
@@ -1,4 +1,53 @@
 const Newsletter = () => {
-	return <h1>Newsletter</h1>;
+	return (
+		<form className="form">
+			<h4 style={{ textAlign: "center", marginBottom: "2rem" }}>
+				our newsletter
+			</h4>
+			<div className="form-row">
+				<label htmlFor="name" className="form-label">
+					first name:
+				</label>
+				<input
+					type="text"
+					className="form-input"
+					name="name"
+					id="name"
+					defaultValue=""
+				/>
+			</div>
+			<div className="form-row">
+				<label htmlFor="lastName" className="form-label">
+					last name:
+				</label>
+				<input
+					type="text"
+					className="form-input"
+					name="lastName"
+					id="lastName"
+					defaultValue=""
+				/>
+			</div>
+			<div className="form-row">
+				<label htmlFor="email" className="form-label">
+					email:
+				</label>
+				<input
+					type="text"
+					className="form-input"
+					name="email"
+					id="email"
+					defaultValue=""
+				/>
+			</div>
+			<button
+				type="submit"
+				className="btn btn-block"
+				style={{ marginTop: "0.5rem" }}
+			>
+				submit
+			</button>
+		</form>
+	);
 };
 export default Newsletter;
