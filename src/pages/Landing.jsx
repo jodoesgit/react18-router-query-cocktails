09a5da962c27274aaf39ef5800572a9d2897d37e
jodoesgit <122350666+jodoesgit@users.noreply.github.com>
2023-09-10 16:26:41 -0700

    Cached search results to Query

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -4,18 +4,31 @@ import Cocktail from "./Cocktail";
 import CocktailList from "../components/CocktailList";
 import SearchForm from "../components/SearchForm";
 
+import { useQuery } from "@tanstack/react-query";
+
+const searchCocktailsQuery = (searchTerm) => {
+	return {
+		queryKey: ["search", searchTerm || "all"],
+		queryFn: async () => {
+			const response = await axios.get(`${cocktailSearchUrl}${searchTerm}`);
+			return response.data.drinks;
+		},
+	};
+};
+
 const cocktailSearchUrl =
 	"https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
 
 export const loader = async ({ request }) => {
 	const url = new URL(request.url);
 	const searchTerm = url.searchParams.get("search") || "";
-	const response = await axios.get(`${cocktailSearchUrl}${searchTerm}`);
-	return { drinks: response.data.drinks, searchTerm };
+
+	return { searchTerm };
 };
 
 const Landing = () => {
-	const { drinks, searchTerm } = useLoaderData();
+	const { searchTerm } = useLoaderData();
+	const { data: drinks } = useQuery(searchCocktailsQuery(searchTerm));
 	return (
 		<>
 			<SearchForm searchTerm={searchTerm} />
